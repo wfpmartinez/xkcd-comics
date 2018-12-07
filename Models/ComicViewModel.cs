0915b15ee99a0344214ce25b8053af209f674184
William Palacios <wfpmartinez@gmail.com>
2018-12-07 14:37:19 -0600

    Adds comic view model class
    Signed-off-by: William Palacios <wfpmartinez@gmail.com>

---
+++ Models/ComicViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace xkcd.Models
+{
+    public class ComicViewModel
+    {
+        public string AlternateText { get; set; }
+        public int Day { get; set; }
+        public string ImageUrl { get; set; }
+        public int Month { get; set; }
+        public string News { get; set; }
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public string Transcript { get; set; }
+    }
+}
