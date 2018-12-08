f71a19d56b46a2e2c4e7a2bbdbfec2efe70808c1
William Palacios <wfpmartinez@gmail.com>
2018-12-07 21:03:21 -0600

    Adds ComicViewModel class with Json serialization contracts.
    Signed-off-by: William Palacios <wfpmartinez@gmail.com>

--- Models/ComicViewModel.cs
+++ Models/ComicViewModel.cs
@@ -1,16 +1,26 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace xkcd.Models
 {
+    [DataContract]
     public class ComicViewModel
     {
-        public string AlternateText { get; set; }
+        [DataMember(Name = "alt")]
+        public string AlternatativeText { get; set; }
+        [DataMember(Name = "day")]
         public int Day { get; set; }
+        [DataMember(Name = "img")]
         public string ImageUrl { get; set; }
+        [DataMember(Name = "month")]
         public int Month { get; set; }
+        [DataMember(Name = "news")]
         public string News { get; set; }
+        [DataMember(Name = "num")]
         public int Number { get; set; }
+        [DataMember(Name = "title")]
         public string Title { get; set; }
+        [DataMember(Name = "transcript")]
         public string Transcript { get; set; }
     }
 }
